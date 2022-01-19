4140339a9a45da009cd6e3530225fe4ae3387dad
agora <adrian.gora14@gmail.com>
2022-01-19 20:55:32 +0100

    adding useInterval hook

---
+++ hooks/useInterval.ts
@@ -0,0 +1,21 @@
+/* eslint-disable consistent-return */
+/* eslint-disable import/prefer-default-export */
+import { useEffect, useRef } from 'react';
+
+export const useInterval = (callback: () => void, delay?: number): void => {
+  const savedCallback = useRef(callback);
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    if (delay === undefined) {
+      return;
+    }
+
+    const id = setInterval(() => savedCallback.current(), delay);
+
+    return () => clearInterval(id);
+  }, [delay]);
+};
