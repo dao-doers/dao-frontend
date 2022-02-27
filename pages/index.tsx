e80925b13e75933917c010673797c10e56caf038
agora <adrian.gora14@gmail.com>
2022-02-27 21:48:41 +0100

    feature: add wave svg to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,16 +10,24 @@ import About from 'sections/homepage/About/About';
 import LastProposals from 'sections/homepage/LastProposals/LastProposals';
 import LastVotes from 'sections/homepage/LastVotes/LastVotes';
 
+const sectionStyle = {
+  backgroundImage: "url('/logos/wave.svg') ",
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: 'top',
+};
+
 const HomePage: FC<NextPage> = () => {
   return (
-    <Layout>
-      <Box sx={{ pl: { xs: 0, md: 6 } }}>
-        <Intro />
-        <About />
-        <LastProposals />
-        <LastVotes />
-      </Box>
-    </Layout>
+    <Box minHeight="100vh" maxWidth="1920px" margin="0 auto" style={sectionStyle}>
+      <Layout>
+        <Box sx={{ pl: { xs: 0, md: 6 } }}>
+          <Intro />
+          <About />
+          <LastProposals />
+          <LastVotes />
+        </Box>
+      </Layout>
+    </Box>
   );
 };
 
