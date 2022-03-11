69c3f3b9b203dcb00ba88dbb59efb5e27edc952d
dziobakwszafie <dziobakwszafie@gmail.com>
2022-03-11 23:18:01 +0100

    fix: build error

--- validators/isAddress.ts
+++ validators/isAddress.ts
@@ -1,11 +1,13 @@
-import * as yup from 'yup';
-import Web3 from 'web3';
+// import * as yup from 'yup';
+// import Web3 from 'web3';
 
-yup.addMethod(yup.string, 'metamaskAddress', function (errorMessage: string) {
-  return this.test('metamask-address', errorMessage, function (value) {
-    const { path, createError } = this;
-    const isValidAddress = Web3.utils.isAddress(value);
+// yup.addMethod(yup.string, 'metamaskAddress', function (errorMessage: string) {
+//   return this.test('metamask-address', errorMessage, function (value) {
+//     const { path, createError } = this;
+//     const isValidAddress = Web3.utils.isAddress(value);
 
-    return isValidAddress ? value : createError({ path, message: errorMessage });
-  });
-});
+//     return isValidAddress ? value : createError({ path, message: errorMessage });
+//   });
+// });
+
+export {};
