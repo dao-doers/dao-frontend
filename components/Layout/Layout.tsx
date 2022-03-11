cda7de725d0184fd1bb53885b64084eb90c97ca9
dziobakwszafie <dziobakwszafie@gmail.com>
2022-03-11 18:13:17 +0100

    feature: refactor checking users shares

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -13,7 +13,6 @@ import BlockchainStatus from 'components/BlockchainStatus/BlockchainStatus';
 import LoadingPage from 'components/LoadingPage/LoadingPage';
 
 import { setTheme } from 'redux/slices/theme';
-import { selectUserAddress, selectIsLoggedIn, setUserShares } from 'redux/slices/user';
 
 import useFetchProposals from 'hooks/useFetchProposals';
 import useFetchVotes from 'hooks/useFetchVotes';
@@ -42,11 +41,7 @@ const Layout: FC<LayoutProps> = ({ children }) => {
 
   const loadingProposals = useFetchProposals();
   const loadingVotes = useFetchVotes();
-  const userAddress = useSelector(selectUserAddress);
-  const isLoggedIn = useSelector(selectIsLoggedIn);
-
-  const fetchMembers = useFetchMembers();
-
+  useFetchMembers();
   useMaintainSession();
 
   useEffect(() => {
@@ -54,30 +49,6 @@ const Layout: FC<LayoutProps> = ({ children }) => {
 
     dispatch(setTheme(theme === THEME_MODES.DARK ? THEME_MODES.DARK : THEME_MODES.LIGHT));
   }, [dispatch]);
-
-  useEffect(() => {
-    if (!fetchMembers.loading && fetchMembers && fetchMembers.data && isLoggedIn) {
-      // TODO: uncomment after new godwoken
-      // const user = fetchMembers.data.members.filter((a: any) => {
-      //   return a.memberAddress === userAddress;
-      // });
-      // if (user[0]) {
-      //   dispatch(setUserShares(user[0].shares));
-      // }
-      // TODO: remove after new godwoken
-      const address = '0xD173313A51f8fc37BcF67569b463abd89d81844f';
-      if (userAddress === address.toLocaleLowerCase()) {
-        const user = fetchMembers.data.members.filter((a: any) => {
-          return a.memberAddress === '0x8016dcd1af7c8cceda53e4d2d2cd4e2924e245b6';
-        });
-        if (user[0]) {
-          dispatch(setUserShares(user[0].shares));
-        }
-      } else {
-        dispatch(setUserShares(0));
-      }
-    }
-  }, [fetchMembers.loading, fetchMembers.data, isLoggedIn]);
 
   return (
     <StyledContainer>
