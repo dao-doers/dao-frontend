63e29e76f5d7fd0b486b3a13347128352ff01b8d
agora <adrian.gora14@gmail.com>
2022-03-23 23:47:47 +0100

    build: fix can not resolve fs

--- next.config.js
+++ next.config.js
@@ -27,4 +27,15 @@ module.exports = {
       },
     ];
   },
+  webpack: (config) => {
+    config.resolve = {
+      ...config.resolve,
+      fallback: {
+        "fs": false,
+        "path": false,
+        "os": false,
+      }
+    }
+    return config
+  },
 };
