648af5af1d61089fedbeb1f4c7ceba5bf55c78b4
agora <adrian.gora14@gmail.com>
2022-03-30 20:28:38 +0200

    feature: add bignumber helper

---
+++ utils/bignumber.js
@@ -0,0 +1,25 @@
+import BigNumber from 'bignumber.js';
+
+export const bigNumberToString = (value) => {
+  return new BigNumber(value).toString();
+};
+
+export const bigNumberSubtract = (value, subtractValue) => {
+  return new BigNumber(value).minus(subtractValue).abs().toString();
+};
+
+export const convertFromCogs = (cogs, decimals) => {
+  return new BigNumber(cogs).dividedBy(10 ** decimals).toString();
+};
+
+export const isValueGreaterThanProvided = (value, providedValue) => {
+  return new BigNumber(value).gt(providedValue);
+};
+
+export const isValueLessThanProvided = (value, providedValue) => {
+  return new BigNumber(value).lt(providedValue);
+};
+
+export const convertToValueFromPercentage = (value, percentage) => {
+  return new BigNumber(value).times(percentage).dividedBy(100).toString();
+};
