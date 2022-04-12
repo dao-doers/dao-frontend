685d2601f714c1ee94975cd4664927f08dc1b000
agora <adrian.gora14@gmail.com>
2022-04-13 00:21:59 +0200

    wip stepper

--- modals/WireddCKBModal/WireddCKBModal.tsx
+++ modals/WireddCKBModal/WireddCKBModal.tsx
@@ -1,4 +1,4 @@
-import { FC, useRef } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import styled from '@emotion/styled';
@@ -11,6 +11,7 @@ import Step from 'components/Steper/Step';
 
 import { selectOpen, setClose } from 'redux/slices/modalWireddCKB';
 
+import { selectUserAddressLayer2 } from 'redux/slices/user';
 import CreateAccountStep from './WireddCKBModalSteps/CreateAccountStep';
 import GetCKBStep from './WireddCKBModalSteps/GetCKBStep';
 import DepositCKBStep from './WireddCKBModalSteps/DepositCKBStep';
@@ -26,6 +27,9 @@ const StyledBox = styled(Box)`
 
 const WireddCKBModal: FC = () => {
   const childRef = useRef<any>(null);
+  const [activeStep, setActiveStep] = useState(0);
+  const [completedSteps, setCompletedSteps] = useState([0, 0, 0, 0]);
+  const [initialRender, setInitialRender] = useState(true);
 
   const dispatch = useDispatch();
 
@@ -35,12 +39,61 @@ const WireddCKBModal: FC = () => {
     dispatch(setClose());
   };
 
+  const nextStep = (): void => {
+    setActiveStep(prevState => {
+      if (prevState === completedSteps.length - 1) {
+        return 0;
+      }
+      return prevState + 1;
+    });
+  };
+
+  const previousStep = (): void => {
+    setActiveStep(prevState => {
+      return prevState - 1;
+    });
+  };
+
+  const completeStep = (): void => {
+    const newState = completedSteps.slice();
+    newState[activeStep] = 1;
+    setCompletedSteps(newState);
+  };
+
+  useEffect(() => {
+    if (initialRender) {
+      setInitialRender(false);
+    } else {
+      nextStep();
+    }
+  }, [JSON.stringify(completedSteps)]);
+
+  const Layer2Address = useSelector(selectUserAddressLayer2);
+
   return (
     <Modal isOpen={isModalOpen} handleClose={handleModalOpen} title="Get dCKB" divider>
       <StyledBox>
-        <Stepper nonLinear interactive ref={childRef}>
+        <Stepper
+          onStepChange={(activeStep, previousStep) => {
+            console.log('Changed from step ', previousStep, ' to step ', activeStep, '.');
+            if (activeStep === 1) {
+              if (Layer2Address) {
+                setCompletedSteps([1, 0, 0, 0])
+                setActiveStep(1);
+              }
+            } else if (activeStep === 2) {
+            } else {
+            }
+          }}
+          nonLinear
+          interactive
+          ref={childRef}
+          activeStep={activeStep}
+          completedSteps={completedSteps}
+          onComplete={() => console.log('Submitted!')}
+        >
           <Step label="Get Layer 2 address">
-            <CreateAccountStep handleNextStep={() => childRef.current.nextStep()} />
+            <CreateAccountStep handleNextStep={() => nextStep()} />
           </Step>
 
           <Step label="Get CKB">
